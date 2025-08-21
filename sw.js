5c11d284183443876df8ff4e608498cbed0f8070
Yeeeetmaster69 <your-email@example.com>
2025-08-21 15:44:20 -0600

    meta charset=UTF-8 + cachebust to fix icon mojibake

--- sw.js
+++ sw.js
@@ -4,3 +4,5 @@ self.addEventListener("activate", () => self.clients.claim());
 // cachebust: 2025-08-20T23:24:48
 
 // cachebust-usersbtn: 2025-08-21T15:40:59
+
+// cachebust-utf8: 2025-08-21T15:44:20
