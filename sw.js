280c6fc619ce8e8b4bc6a1d89ec91e93ff18918c
Yeeeetmaster69 <teamyeeeet@gmail.com>
2025-08-24 16:12:21 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-﻿self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
-
-// cachebust: 2025-08-20T23:24:48
-
-// cachebust-usersbtn: 2025-08-21T15:40:59
-
-// cachebust-utf8: 2025-08-21T15:44:20
